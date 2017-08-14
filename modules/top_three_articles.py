e277586922a02e37b6469b8562329bc8e346414a
Robert Dickerson <rdickeriii@gmail.com>
2017-08-14 12:48:40 -0700

    Refactor: Small fixes.
    end_of_line

--- modules/top_three_articles.py
+++ modules/top_three_articles.py
@@ -1,5 +1,5 @@
 from modules import connection
-from modules import formatter
+# from modules import formatter
 
 
 def get_top_three_articles():
@@ -15,12 +15,12 @@ def get_top_three_articles():
     return cursor.fetchall()
 
 
-def format_top_three_articles():
-    print("Top articles:")
-    formatter.repeat_separator()
-    for item in get_top_three_articles():
-        print("The total views for the article '" + str(item[2]) +
-              "', by the author '" + str(item[3]) +
-              "' on the page '" + str(item[1]) +
-              "' are " + formatter.format_num(item[0]) + '.')
-    formatter.repeat_separator()
+# def format_top_three_articles():
+#     print("Top articles:")
+#     formatter.repeat_separator()
+#     for item in get_top_three_articles():
+#         print("The total views for the article '" + str(item[2]) +
+#               "', by the author '" + str(item[3]) +
+#               "' on the page '" + str(item[1]) +
+#               "' are " + formatter.format_num(item[0]) + '.')
+#     formatter.repeat_separator()
