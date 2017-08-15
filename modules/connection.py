27a09cf462624bf788857998f06bcce5ab7e4801
Robert Dickerson <rdickeriii@gmail.com>
2017-08-15 13:37:09 -0700

    Refactor: created requirements.txt
    end_of_line

--- modules/connection.py
+++ modules/connection.py
@@ -2,5 +2,5 @@ import psycopg2
 
 
 def get_connection():
-    db = psycopg2.connect("database=postgres user=postgres password=Hellfreeze1")
+    db = psycopg2.connect("dbname=postgres user=postgres password=Hellfreeze1")
     return db.cursor()
