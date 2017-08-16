f7afb0b66bd6a93efda9a08c34722893ef775a66
Robert Dickerson <rdickeriii@gmail.com>
2017-08-16 12:04:39 -0700

    Refactor: I don't understand why the tuple is wrong
    end_of_line

--- modules/top_three_articles.py
+++ modules/top_three_articles.py
@@ -4,23 +4,23 @@ from modules import formatter
 
 def get_top_three_articles():
     cursor = connection.get_connection()
-    top_three_articles = """
+    top_three_articles = ("""
         SELECT title, path, count(path) AS hits
         FROM articles, log
         WHERE log.path = concat('/article/', articles.slug)
         GROUP BY title, path
         ORDER BY hits DESC LIMIT 3;
-    """
+    """)
     cursor.execute(top_three_articles)
-    return cursor.fetchall()
+    return cursor
 
 
 def print_top_three_articles():
     print("Top articles:")
     formatter.repeat_separator()
-    for item in get_top_three_articles():
-        print("The total views for the article '" + str(item[2]) +
-              "', by the author '" + str(item[3]) +
-              "' on the page '" + str(item[1]) +
-              "' are " + formatter.format_num(item[0]) + '.')
+    for items in get_top_three_articles():
+        print("The total views for the article '" + str(items[2]) +
+              "', by the author '" + str(items[3]) +
+              "' on the page '" + str(items[1]) +
+              "' are " + formatter.format_num(items[0]) + '.')
     formatter.repeat_separator()
