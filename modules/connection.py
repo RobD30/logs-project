bef73cd3935a28184dd8e25bf50e178a6586cd7e
Robert Dickerson <rdickeriii@gmail.com>
2017-08-16 11:13:22 -0700

    Refactor: changed password
    end_of_line

--- modules/connection.py
+++ modules/connection.py
@@ -2,5 +2,5 @@ import psycopg2
 
 
 def get_connection():
-    db = psycopg2.connect("dbname=postgres user=postgres password=Hellfreeze1")
+    db = psycopg2.connect("dbname=postgres user=postgres password=postgres")
     return db.cursor()
