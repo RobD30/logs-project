c2ce0c057bbea55ab6e95ee81d9c009bc87f77fa
Robert Dickerson <rdickeriii@gmail.com>
2017-08-19 15:46:26 -0700

    Refactor: Many Fixes
    end_of_line

--- modules/error_logs.py
+++ modules/error_logs.py
@@ -1,32 +1,46 @@
 from modules import connection
 from modules import formatter
 
+def main():
+    print_error_logs()
+
 
 def get_status_log():
     cursor = connection.get_connection()
     # You need to join the logs to the path
     error_reports = """
-        SELECT time::DATE,
+        WITH t AS (
+        SELECT time::DATE AS time,
         SUM (
             CASE WHEN status = '200 OK'
             THEN 1
             ELSE 0
             END) AS success,
         SUM (
-            CASE WHEN status  =  '404 NOT FOUND.'
+            CASE WHEN status  =  '404 NOT FOUND'
             THEN 1
             ELSE 0
-            END) AS error
-        FROM log GROUP BY time::DATE;
+            END) AS error,
+            log.path
+        FROM log
+        GROUP BY time::DATE, path)
+        SELECT t.time, t.error, t.path, t.success
+        FROM t
+        JOIN articles
+        ON t.path =
+        CONCAT('/article/', articles.slug);
     """
     cursor.execute(error_reports)
     return cursor.fetchall()
 
 
 def print_error_logs():
-    print("Top articles:")
+    print("Errors:")
     formatter.repeat_separator()
-    for item in get_status_log():
+    for error in get_status_log():
         pass
 
     formatter.repeat_separator()
+
+if __name__ == '__main__':
+    main()
